43937935d2ab59cd0fa631d8032c955f6d6c0f17
YellowFish <theo.costantini@gmail.com>
2016-04-25 17:14:56 +0200

    Add Human constructors

--- src/com/volterra/ecosysteme/Human.java
+++ src/com/volterra/ecosysteme/Human.java
@@ -4,5 +4,26 @@ package com.volterra.ecosysteme;
  * Created by YellowFish on 25/04/2016.
  */
 public class Human extends Species {
-    
+    /**
+     * Default constructor. Calls Species default constructor
+     */
+    public Human() {
+        super();
+    }
+
+    /**
+     * Constructor with all the parameters
+     * @param friendlySpecies
+     * @param averageLifeSpan
+     * @param litterSize
+     * @param aggressiveness
+     * @param force
+     * @param reproductivity
+     * @param mutualAid
+     * @param courage
+     * @param speed
+     */
+    public Human(Species[] friendlySpecies, int averageLifeSpan, int litterSize, int aggressiveness, int force, int reproductivity, int mutualAid, int courage, float speed) {
+        super(friendlySpecies, averageLifeSpan, litterSize, aggressiveness, force, reproductivity, mutualAid, courage, speed);
+    }
 }
