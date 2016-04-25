e72c5c9a83f5e78c052d152c66e4e09680da4c95
KrisNumber24 <assante.christophe@laposte.net>
2016-04-25 17:48:56 +0200

    Fix some bugs in Trait

--- src/com/volterra/ecosysteme/Trait.java
+++ src/com/volterra/ecosysteme/Trait.java
@@ -2,13 +2,19 @@ package com.volterra.ecosysteme;
 
 /**
  * Created by Christophe on 25/04/2016.
+ *
+ * Décorateur pour ajouter un trait de personnalité aux tribus.
  */
-public abstract class Trait extends AbstractTribe {
-    /* La Tribe à laquelle on ajoute le décorateur */
-    protected AbstractTribe _originalTribe;
+public abstract class Trait extends Tribe {
+    /**
+     * La tribu à laquelle on ajoute le décorateur
+     */
+    protected Tribe _originalTribe;
 
-    /* La fonction qui réalise l'action à modifier en fonction de la classe AbstractTribe */
-    public void computeAction() {
+    /**
+     * Distribue les bonus et les malus liés au trait
+     */
+    public void computeAttributes() {
 
     }
 }
