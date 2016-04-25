341ed584734e07391ec3ae95f7caf9a594a5a007
Victor Viale <somebodywas@gmail.com>
2016-04-25 21:01:35 +0200

    Move AI states to interface AIStateMachine

--- src/com/volterra/ecosysteme/AIStateMachine.java
+++ src/com/volterra/ecosysteme/AIStateMachine.java
@@ -4,5 +4,7 @@ package com.volterra.ecosysteme;
  * Created by Victor on 25/04/2016.
  */
 public interface AIStateMachine {
+  enum State { NEUTRAL, MIGRATING, FLEEING, AGGRESSING };
+
   void runAI(float deltaTime);
 }
