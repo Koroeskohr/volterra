1d0707cffa92752da49f05aa85f6f0e6873f326a
YellowFish <theo.costantini@gmail.com>
2016-04-26 15:27:54 +0200

    Add FIGHT state for Tribes

--- src/com/volterra/ecosysteme/AIStateMachine.java
+++ src/com/volterra/ecosysteme/AIStateMachine.java
@@ -4,7 +4,7 @@ package com.volterra.ecosysteme;
  * Created by Victor on 25/04/2016.
  */
 public interface AIStateMachine {
-  enum State { NEUTRAL, MIGRATING, FLEEING, AGGRESSING };
+  enum State { NEUTRAL, MIGRATING, FLEEING, AGGRESSING, FIGHT };
 
   void runAI(float deltaTime);
 }
