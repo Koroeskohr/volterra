28644275822eb93b818f5a36745a61af291c1c3b
Victor Viale <somebodywas@gmail.com>
2016-05-05 02:13:56 +0200

    Better fade out formula for damage vfx

--- src/com/volterra/Engine/visualeffects/DamageEffect.java
+++ src/com/volterra/Engine/visualeffects/DamageEffect.java
@@ -38,7 +38,8 @@ public class DamageEffect extends VisualEffect {
   public void render(PApplet ctx){
     // Goes up to 500, window is 720px high
     long timeTillSpawn = this.creationTime.until(Instant.now(), ChronoUnit.MILLIS);
-    ctx.fill(255, 100, 100, Utils.clamp(300 - timeTillSpawn/2, 0, 255));
+
+    ctx.fill(255, 100, 100, Utils.clamp(400 - timeTillSpawn, 0, 255));
     ctx.textSize(15);
     ctx.text("-" + damageValue, x, y - (timeTillSpawn / 18));
 
