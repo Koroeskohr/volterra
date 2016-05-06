59b42f6ff805bf4c36463e3c05425954795dcecb
Victor Viale <somebodywas@gmail.com>
2016-05-06 23:19:07 +0200

    Document AIStateMachine, reindent

--- src/com/volterra/ecosysteme/AIStateMachine.java
+++ src/com/volterra/ecosysteme/AIStateMachine.java
@@ -4,7 +4,14 @@ package com.volterra.ecosysteme;
  * Created by Victor on 25/04/2016.
  */
 public interface AIStateMachine {
+  /**
+   * Different states of a Tribe
+   */
   enum State { NEUTRAL, MIGRATING, FLEEING, AGGRESSING, FIGHT };
 
+  /**
+   * Updates AI
+   * @param deltaTime Time elapsed since last frame
+   */
   void runAI(float deltaTime);
 }
