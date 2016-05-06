f50e0c7b27039de4861b4d893120d148d4b52052
Victor Viale <somebodywas@gmail.com>
2016-05-06 23:19:23 +0200

    Document Renderable, reindent

--- src/com/volterra/ecosysteme/Renderable.java
+++ src/com/volterra/ecosysteme/Renderable.java
@@ -5,17 +5,17 @@ import processing.core.PApplet;
 /**
  * Created by Victor on 25/04/2016.
  *
- * Define necessary methods to draw an element
+ * Defines necessary methods to draw an element
  */
 public interface Renderable {
   /**
-   * Update the state of the element to draw.
-   * @param deltaTime
+   * Updates the state of the element to draw.
+   * @param deltaTime Time elapsed since last frame
      */
   void update(float deltaTime);
 
   /**
-   * Draw the element on the screen
+   * Draws the element on the screen
    * @param ctx The context which draws the element. Here it is a PApplet.
      */
   void render(PApplet ctx);
