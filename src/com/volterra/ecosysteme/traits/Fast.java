f9cb44991cd01a08c60cbc5ff531f5edb321fa0c
Victor Viale <somebodywas@gmail.com>
2016-05-06 15:05:22 +0200

    Document Fast trait

--- src/com/volterra/ecosysteme/traits/Fast.java
+++ src/com/volterra/ecosysteme/traits/Fast.java
@@ -9,21 +9,29 @@ import com.volterra.ecosysteme.Tribe;
  * Add the <i>Fast</i> trait to a <i>Tribe</i>.
  */
 public class Fast<T extends Species> extends Trait<T> {
-    /**
-     * Constructor
-     * @param originalTribe The <i>Tribe</i> on wich apply the trait.
-     */
-    public Fast(Tribe<T> originalTribe) {
-        super(originalTribe);
-    }
+  /**
+   * Constructor
+   * @param originalTribe The <i>Tribe</i> on which apply the trait.
+   */
+  public Fast(Tribe<T> originalTribe) {
+    super(originalTribe);
+  }
 
-    @Override
-    public float getSpeed() {
-        return this._originalTribe.getSpeed() + 0.2f;
-    }
+  /**
+   * @see Trait#getSpeed()
+   * @return Speed with computed decorators
+   */
+  @Override
+  public float getSpeed() {
+    return this._originalTribe.getSpeed() + 0.2f;
+  }
 
-    @Override
-    public float getAttackSpeed() {
-        return this._originalTribe.getAttackSpeed() + 0.2f;
-    }
+  /**
+   * @see Trait#getAttackSpeed()
+   * @return Attack speed with computed decorators
+   */
+  @Override
+  public float getAttackSpeed() {
+    return this._originalTribe.getAttackSpeed() + 0.2f;
+  }
 }
